32367f62bf627f056301c8e6bd10ebf23f70e900
zilezarach <zarachiistephen@gmail.com>
2024-08-06 18:43:23 +0300

    orderpage

--- src/app/Orders/page.tsx
+++ src/app/Orders/page.tsx
@@ -1,7 +1,27 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 export default function Page() {
   type Order = {
@@ -23,11 +43,56 @@ export default function Page() {
   const [order, setOrder] = useState<Order[]>([]);
   const [weeklyData, setWeeklyData] = useState<number[]>([]);
 
+  const processWeeklyData = (order: any[]) => {
+    const weeklyTransactions = Array(7).fill(0);
+    const now = new Date();
+    const startOfWeek = new Date(now.setDate(now.getDate() - now.getDay()));
+
+    order.forEach((order) => {
+      const OrderDate = new Date(order.date);
+      const diffDays = Math.floor(
+        (OrderDate.getTime() - startOfWeek.getTime()) / (1000 * 60 * 60 * 24),
+      );
+      if (diffDays >= 0 && diffDays < 7) {
+        weeklyTransactions[diffDays]++;
+      }
+    });
+
+    setWeeklyData(weeklyTransactions);
+  };
+
+  const chartData = {
+    labels: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+    datasets: [
+      {
+        label: "Orders",
+        data: weeklyData,
+        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+      },
+    ],
+  };
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
   return (
     <div className=" min-h-screen container-none mx-auto bg-gray-300 px-4 ">
       <h1 className="text-center texl-2xl text-rose-600 mb-6 mt-6 font-bold no-underline hover:underline">
         Orders
       </h1>
+      <div className="mb-8 shadow-md rounded  bg-white style={{ height: '400px' }}">
+        <Line data={chartData} options={chartOptions} />
+      </div>
       <div className="flex items-center justify-center">
         <table className=" table-auto min-w-full bg-white shadow-md rounded ml-4 mr-4">
           <thead>
