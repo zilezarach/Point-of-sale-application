a46944fc3a8eff70efe5a29effe2ccb4fe79a0b5
zilezarach <zarachiistephen@gmail.com>
2025-01-13 21:11:58 +0300

    change to production9

--- src/app/Payment/page.tsx
+++ src/app/Payment/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router"; // Import useRouter
 import dynamic from "next/dynamic";
 
 // Dynamically import the PaymentForm component with no SSR
@@ -10,14 +9,13 @@ const PaymentForm = dynamic(() => import("@/components/paymentform"), {
 });
 
 export default function Payment() {
-  const router = useRouter(); // Use useRouter instead of useSearchParams
   const [total, setTotal] = useState<number | null>(null);
 
   useEffect(() => {
-    // Access the query parameters via router.query
-    const totalPrice = parseFloat((router.query.total as string) || "0");
+    const params = new URLSearchParams(window.location.search); // Access URL params
+    const totalPrice = parseFloat(params.get("total") || "0");
     setTotal(totalPrice);
-  }, [router.query]);
+  }, []);
 
   if (total === null) {
     return <div>Loading...</div>;
