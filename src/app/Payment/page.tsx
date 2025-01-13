35040454f0ed04e46ca08fcd59e8d3eed3b492b9
zilezarach <zarachiistephen@gmail.com>
2025-01-13 21:05:50 +0300

    change to production9

--- src/app/Payment/page.tsx
+++ src/app/Payment/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import dynamic from "next/dynamic";";
+import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
 
 const PaymentForm = dynamic(() => import("@/components/paymentform"), {
