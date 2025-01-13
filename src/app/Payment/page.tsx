368dfb714f105d6a77292ab0a1768e7be3910104
zilezarach <zarachiistephen@gmail.com>
2025-01-13 19:51:04 +0300

    change to production3

--- src/app/Payment/page.tsx
+++ src/app/Payment/page.tsx
@@ -1,14 +1,17 @@
-'use client'
+"use client";
 
 import React from "react";
-import PaymentForm from '@/components/paymentform';
+import PaymentForm from "@/components/paymentform";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function Payment() {
   const searchParams = useSearchParams();
-  const total = parseFloat(searchParams.get('total') || '0');
+  const total = parseFloat(searchParams.get("total") || "0");
 
   return (
-    <PaymentForm total={total} />
-  )
+    <Suspense>
+      <PaymentForm total={total} />
+    </Suspense>
+  );
 }
