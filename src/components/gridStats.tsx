d45cb9f6ba1737e63e8993d6e212e6626c5c2f8a
zilezarach <zarachiistephen@gmail.com>
2025-01-13 19:05:16 +0300

    change to production1

--- src/components/gridStats.tsx
+++ src/components/gridStats.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { IoBagHandle } from 'react-icons/io5';
+import { IoBagHandle } from "react-icons/io5";
 
-const gridStats = ({ totalAmount }) => {
-  function BoxWrapper({ children }) {
-    return <div className="bg-white rounded-full p-1 flex-1 border border-rose-200 flex items-center mb-6 ml-3 mr-3 mt-3">{children}</div>
+interface GridTops {
+  totalAmount: number;
+}
+
+const gridStats = ({ totalAmount }: GridTops) => {
+  function BoxWrapper({ children }: { children: React.ReactNode }) {
+    return (
+      <div className="bg-white rounded-full p-1 flex-1 border border-rose-200 flex items-center mb-6 ml-3 mr-3 mt-3">
+        {children}
+      </div>
+    );
   }
 
   return (
@@ -13,15 +21,19 @@ const gridStats = ({ totalAmount }) => {
           <IoBagHandle className="text-2xl text-white" />
         </div>
         <div className="pl-4">
-          <span className="text-sm text-gray-500 font-light">Total Amount This Week</span>
+          <span className="text-sm text-gray-500 font-light">
+            Total Amount This Week
+          </span>
           <div className="flex items-center">
-            <strong className="text-xl text-gray-700 font-semibold">${totalAmount.toFixed(2)}</strong>
+            <strong className="text-xl text-gray-700 font-semibold">
+              ${totalAmount.toFixed(2)}
+            </strong>
           </div>
         </div>
       </BoxWrapper>
       {/* Add other BoxWrapper components if needed */}
     </div>
   );
-}
+};
 
 export default gridStats;
