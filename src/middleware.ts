fe6d99b97e8261458877f3e3a1a0c1dc12db9663
zilezarach <zarachiistephen@gmail.com>
2025-02-02 02:07:33 +0300

    Fixed Api for production

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,31 +9,43 @@ interface MyJwtPayload extends JwtPayload {
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
+  console.log("Middleware triggered for:", pathname);
 
   const protectedRoutes = ["/admin", "/POs"];
-
   if (protectedRoutes.some((route) => pathname.startsWith(route))) {
     const token = req.cookies.get("token")?.value;
+    console.log("Token from cookie:", token);
+
     if (!token) {
+      console.log("No token found. Redirecting to /Shop.");
       return NextResponse.redirect(new URL("/Shop", req.url));
     }
     try {
-      const decode = jwt.verify(token, process.env.JWT_SECRET!) as MyJwtPayload;
-
-      if (decode && decode.role) {
-        const userRole = decode.role.toLowerCase();
+      const decoded = jwt.verify(
+        token,
+        process.env.JWT_SECRET!,
+      ) as MyJwtPayload;
+      console.log("Decoded token:", decoded);
+      if (decoded && decoded.role) {
+        const userRole = decoded.role.toLowerCase();
         if (userRole !== "admin" && userRole !== "employee") {
-          return NextResponse.redirect(new URL("/Shop", req.url));
-        } else {
+          console.log("User role not authorized:", userRole);
           return NextResponse.redirect(new URL("/Shop", req.url));
         }
+      } else {
+        console.log("Token does not have a role field. Redirecting.");
+        return NextResponse.redirect(new URL("/Shop", req.url));
       }
     } catch (error) {
+      console.log("Error verifying token:", error);
       return NextResponse.redirect(new URL("/Shop", req.url));
     }
+    console.log("Token verified. Proceeding to protected route.");
     return NextResponse.next();
   }
+  return NextResponse.next();
 }
+
 export const config = {
   matcher: ["/admin/:path*", "/POs/:path*"],
 };
