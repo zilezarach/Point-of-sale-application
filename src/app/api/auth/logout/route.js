7347367f355a4744afceb4554b130b25dd65e172
zilezarach <zarachiistephen@gmail.com>
2025-02-02 03:29:45 +0300

    Fixed Api for production

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  // Invalidate the session or token here
-  // For example, you might clear a session cookie or invalidate a JWT
-
-  // Clear the cookie (assuming you use a cookie-based session)
-  const response = NextResponse.json({ message: "Logged out successfully" });
-  response.cookies.delete("sessionToken"); // Replace with your cookie name
+  const response = NextResponse.json({ message: "logged out" });
+  response.cookies.set("token", "", {
+    path: "/",
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 0, // Immediately expire the cookie
+  });
   return response;
 }
